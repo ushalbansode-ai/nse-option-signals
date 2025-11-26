fd9246797a1e42921e462e875c9503d5452a16d6
ushalbansode-ai <ushalbansode@gmail.com>
2025-11-26 15:49:57 +0530

    Update parse_dat_bhav.py

--- src/parse_dat_bhav.py
+++ src/parse_dat_bhav.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import io
+import zipfile
 
+def parse_dat_bytes(raw_bytes):
+    with zipfile.ZipFile(io.BytesIO(raw_bytes)) as z:
+        fname = z.namelist()[0]
+        df = pd.read_csv(z.open(fname))
+
+    # Clean field names
+    df.columns = df.columns.str.strip()
+
+    return df
+  
